cb61471b9b885d472c3c488013fbc76cdec7ed83
Udit Gupta <alugupta@gmail.com>
2018-04-13 16:49:00 -0400

    Initial DeepSpeech 2 benchmark push.

---
+++ deepspeech2_speechrecognition/download_dataset.sh
@@ -0,0 +1,4 @@
+# Script to download Librispeech Dataset
+#for script testing, only fetch minimal, clean dataset
+
+python data/librispeech.py #--files_to_use train-clean-100.tar.gz,dev-clean.tar.gz,test-clean.tar.gz
