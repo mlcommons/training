f7adf1de690db843db64a70077048ccdcf91473d
Victor Bittorf <vbittorf@google.com>
2018-04-18 11:50:17 -0700

    Resnet port in progress. Currently broken.

---
+++ image_classifiction/tensorflow/run_and_time.sh
@@ -0,0 +1,4 @@
+# TODO
+
+
+python3 resnet/imagenet_main.py --data_dir /imn/imagenet/combined/ --model_dir /tmp/imn_example --train_epochs 10000 --stop_threshold 0.749 --batch_size 64 --version 1 --resnet_size 50
