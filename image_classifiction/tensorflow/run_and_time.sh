487bb6ec3d2819acea4f33ca710ac51e138d8b0b
Victor Bittorf <vbittorf@google.com>
2018-04-19 16:45:07 +0000

    Adding dependencies and scripts for resnet.

--- image_classifiction/tensorflow/run_and_time.sh
+++ image_classifiction/tensorflow/run_and_time.sh
@@ -1,4 +1,6 @@
 # TODO
 
+cd /research/resnet
 
-python3 resnet/imagenet_main.py --data_dir /imn/imagenet/combined/ --model_dir /tmp/imn_example --train_epochs 10000 --stop_threshold 0.749 --batch_size 64 --version 1 --resnet_size 50
+export PYTHONPATH=`pwd`:$PYTHONPATH
+python3 official/resnet/imagenet_main.py --data_dir /imn/imagenet/combined/ --model_dir /tmp/imn_example --train_epochs 10000 --stop_threshold 0.749 --batch_size 64 --version 1 --resnet_size 50
