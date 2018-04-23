2d42cf96e713a0efbd5d2dcfd3b7e68786f148a4
Victor Bittorf <vbittorf@google.com>
2018-04-23 14:31:28 -0700

    Added a 3 for python...

--- transformer/download_data.sh
+++ transformer/download_data.sh
@@ -3,4 +3,4 @@
 wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2014.en -O tensorflow/newstest2014.en
 wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2014.de -O tensorflow/newstest2014.de
 
-python data_download.py --raw_dir raw_data
+python3 data_download.py --raw_dir raw_data
