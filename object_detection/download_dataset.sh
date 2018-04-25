8787eae1de8c51aa486eb4f4122f30f6f9470624
Daniel Kang <daniel.d.kang@gmail.com>
2018-04-25 08:17:11 +0000

    download ms-coco

---
+++ object_detection/download_dataset.sh
@@ -0,0 +1,3 @@
+wget https://s3-us-west-2.amazonaws.com/detectron/coco/coco_annotations_minival.tgz
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
