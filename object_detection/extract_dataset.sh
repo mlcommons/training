0f7089815968a961ff4fe308908294e9e1f4b729
Daniel Kang <daniel.d.kang@gmail.com>
2018-04-28 07:48:27 +0000

    Add extract dataset for object detection

---
+++ object_detection/extract_dataset.sh
@@ -0,0 +1,11 @@
+mkdir detectron/lib/datasets/data/coco
+mv coco_annotations_minival.tgz detectron/lib/datasets/data/coco
+mv train2014.zip detectron/lib/datasets/data/coco
+mv val2014.zip detectron/lib/datasets/data/coco
+
+cd detectron/lib/datasets/data/coco
+dtrx --one=here coco_annotations_minival.tgz
+dtrx train2014.zip
+mv train2014/ coco_train2014/
+dtrx val2014.zip
+mv val2014/ coco_val2014/
