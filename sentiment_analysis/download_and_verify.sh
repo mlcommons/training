9715a38fc83e86d7ac9847e87eec92c2a1d8938d
Siddharth Goyal <vi.siddharth78@gmail.com>
2018-04-29 00:21:16 -0700

    Fix download and verify in sentiment_analysis by making it independent of paddle (#12)
    * Fix download and verify, by making it independent of paddle
    
    * Minor fix to import print
    
    * Fix chunk size in download
    
    * Address review comments

--- sentiment_analysis/download_and_verify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Use the python script to download the dataset and 
-# verify the MD5 checksum.
-python paddle/download_and_verify.py
---
+++ sentiment_analysis/verify_dataset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Use the python script to verify the MD5 checksum 
+# of the downloaded dataset.
+python verify.py
