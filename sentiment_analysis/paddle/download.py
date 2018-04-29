5fb5c24f0864ee00d1d9f26401e08b9232265800
sidgoyal78 <vi.siddharth78@gmail.com>
2018-04-28 22:34:46 -0700

    Fix chunk size in download

--- sentiment_analysis/paddle/download.py
+++ sentiment_analysis/paddle/download.py
@@ -9,6 +9,7 @@ URL = 'http://ai.stanford.edu/%7Eamaas/data/sentiment/aclImdb_v1.tar.gz'
 MD5 = '7c2ac02c03563afcf9b574c7e56c153a'
 DIR = os.path.expanduser('~/.cache/paddle/dataset/imdb')
 PATH = os.path.join(DIR, URL.split('/')[-1])
+CHUNK_SIZE = 4096
 
 # Download the dataset
 def download():
@@ -33,7 +34,7 @@ def download():
             with open(PATH, 'w') as f:
                 dl = 0
                 total_length = int(total_length)
-                for data in r.iter_content(chunk_size=4096):
+                for data in r.iter_content(chunk_size=CHUNK_SIZE):
                     dl += len(data)
                     f.write(data)
 
