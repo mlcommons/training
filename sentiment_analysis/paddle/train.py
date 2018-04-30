1651ae22cc210f9b195da22f7b95c387e4c07615
sidgoyal78 <vi.siddharth78@gmail.com>
2018-04-30 00:06:36 -0700

    Clean up code and add comments

--- sentiment_analysis/paddle/train.py
+++ sentiment_analysis/paddle/train.py
@@ -15,22 +15,27 @@ import argparse
 # Define the architecture for the convolution model
 def convolution_net(data, label, input_dim, class_dim=2, emb_dim=1024,
                     hid_dim=1024):
+    # First layer is an embedding layer, which serves as a lookup table for
+    # a one-hot encoding vector.
     emb = fluid.layers.embedding(
         input=data, size=[input_dim, emb_dim], is_sparse=True)
+
+    # Output of embedding layer is fed to 2 independent sequence_conv_pool
+    # layers. The sequence_conv_pool layer is a combination of a sequence- 
+    # convolution layer and a sequence pooling layer
     conv_3 = fluid.nets.sequence_conv_pool(
         input=emb,
         num_filters=hid_dim,
         filter_size=3,
         act="elu",
         pool_type="max")
-        #padding=2)
     conv_4 = fluid.nets.sequence_conv_pool(
         input=emb,
         num_filters=hid_dim,
         filter_size=4,
         act="elu",
         pool_type="max")
-        #padding=3)
+
     prediction = fluid.layers.fc(input=[conv_3, conv_4],
                                  size=class_dim,
                                  act="softmax")
@@ -51,9 +56,11 @@ def stacked_lstm_net(data,
                      stacked_num=3):
     assert stacked_num % 2 == 1
 
+    # First layer is again an embedding layer
     emb = fluid.layers.embedding(
         input=data, size=[input_dim, emb_dim], is_sparse=True)
 
+    # The embedding layer is followed by `stacked_num` FC+LSTM layers
     fc1 = fluid.layers.fc(input=emb, size=hid_dim)
     lstm1, cell1 = fluid.layers.dynamic_lstm(input=fc1, size=hid_dim)
 
@@ -96,8 +103,7 @@ def train(word_dict,
     # Seed for weight initialization
     fluid.default_startup_program().random_seed = seed
 
-    # lod_level stores the offset for the mini_batch. An abstraction on top of tensors to store variable
-    # length sequences.
+    # Setup input features and label as data layers
     data = fluid.layers.data(
         name="words", shape=[1], dtype="int64", lod_level=1)
     label = fluid.layers.data(name="label", shape=[1], dtype="int64")
@@ -105,28 +111,26 @@ def train(word_dict,
     cost, acc_out, prediction = net_method(
         data, label, input_dim=dict_dim, class_dim=class_dim)
 
+    # Initialize a test program for obtaining test accuracy and cost
     test_program = fluid.default_main_program().clone(for_test=True)
 
-
-    #adam = fluid.optimizer.Adam(learning_rate=0.0009)
-
-    """
-    Learning rate of 5e-4 works for conv models and 2e-3 for LSTM models
-    """
-    adam = fluid.optimizer.Adam(learning_rate=0.0005)
-
-    #adam = fluid.optimizer.Adam(learning_rate=0.002, regularization=fluid.regularizer.L2DecayRegularizer(regularization_coeff=0.00005))
+    # Setup Adam optimizer
+    adam = fluid.optimizer.Adam(learning_rate=0.0005) #Learning rate of 5e-4 works for conv models and 2e-3 for LSTM model
 
     optimize_ops, params_grads = adam.minimize(cost)
 
+    # Create reader to iterate over training set
     train_reader = paddle.batch(
         paddle.reader.shuffle(
             paddle.dataset.imdb.train(word_dict), buf_size=25000),
         batch_size=BATCH_SIZE)
+
+    # Setup place and executor for runtime
     place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
     exe = fluid.Executor(place)
     feeder = fluid.DataFeeder(feed_list=[data, label], place=place)
-
+    
+    # Create reader to iterate over validation set
     test_reader = paddle.batch(
                     paddle.dataset.imdb.test(word_dict), batch_size=BATCH_SIZE)
 
@@ -136,8 +140,10 @@ def train(word_dict,
 
         for pass_id in xrange(PASS_NUM):
             train_loss_set = []
-            train_acc_set = []     
-            # Calcuate training loss and accuracy
+            train_acc_set = []  
+   
+            # Calculate average training loss and accuracy
+            # across all mini-batches in the training set
             for batch_id, data in enumerate(train_reader()):
                 cost_val, acc_val = exe.run(main_program,
                                             feed=feeder.feed(data),
@@ -147,7 +153,8 @@ def train(word_dict,
 	    train_loss = np.array(train_loss_set).mean()
             train_acc = np.array(train_acc_set).mean() * 100
 
-            # Calculate valication loss and accuracy
+            # Calculate average valication loss and accuracy 
+            # across all mini-batches in the validation set
             acc_set = []
             avg_loss_set = []
             for tid, test_data in enumerate(test_reader()):
