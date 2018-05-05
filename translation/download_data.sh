9f5001a27ad83dc0177615271d492994c8ecb2d6
Victor Bittorf <vbittorf@google.com>
2018-05-05 11:59:20 -0700

    Fix download links for translation test data. (#33)
    The test data appears to have moved. Updated the links with the current location.

--- translation/download_data.sh
+++ translation/download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/tensorflow/mlbenchmark/fixes/transformer/test_data/newstest2014.en?token=AeMkozrG27bVaqsTvxa8j3it15V2S8_2ks5a6QBUwA -O tensorflow/newstest2014.en
-wget https://raw.githubusercontent.com/tensorflow/mlbenchmark/fixes/transformer/test_data/newstest2014.de?token=AeMko9qQ2IRoST3hDGUenronIm9qPTqwks5a6QDEwA -O tensorflow/newstest2014.de
+wget https://raw.githubusercontent.com/tensorflow/mlbenchmark/master/transformer/test_data/newstest2014.de?token=ABCvMNi6FA6_WuLcXj_UJLv_GUc8I9NWks5a9yfEwA -O tensorflow/newstest2014.en
+wget https://raw.githubusercontent.com/tensorflow/mlbenchmark/master/transformer/test_data/newstest2014.en?token=ABCvMOlxzi69iyZbqRXa0tVuRzSz83v9ks5a9yfIwA -O tensorflow/newstest2014.de
 
 python3 data_download.py --raw_dir raw_data
