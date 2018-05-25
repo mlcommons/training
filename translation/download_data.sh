97ec04164d2796e3200f4827859c54f326396110
Victor Bittorf <vbittorf@google.com>
2018-05-25 09:48:55 -0700

    Fix transformer download.
    The URLs are bad again...

--- translation/download_data.sh
+++ translation/download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/tensorflow/mlbenchmark/master/transformer/test_data/newstest2014.de?token=ABCvMNi6FA6_WuLcXj_UJLv_GUc8I9NWks5a9yfEwA -O tensorflow/newstest2014.en
-wget https://raw.githubusercontent.com/tensorflow/mlbenchmark/master/transformer/test_data/newstest2014.en?token=ABCvMOlxzi69iyZbqRXa0tVuRzSz83v9ks5a9yfIwA -O tensorflow/newstest2014.de
+wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2014.en -O tensorflow/newstest2014.en
+wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2014.de -O tensorflow/newstest2014.de
 
 python3 data_download.py --raw_dir raw_data
