085e913f7c72179ac0b4e625be032d5c2bafec32
Victor Bittorf <vbittorf@google.com>
2018-06-05 15:50:10 -0700

    Fix and cleanup Translation (#61)
    This patch addresses the following issue:
    
    * Unused download script, removed to avoid confusion.
    * Freezes pip versions.
    * Freezes TF version.
    * New download links which should have the correct data and be stable.
    * Adds a verify_dataset script.
    
    Convergence test results:
    Bleu score (uncased): 25.147438049316406
    Bleu score (cased): 24.649880826473236
    ENDING TIMING RUN AT 2018-06-02 05:41:52 AM
    RESULT,transformer,1,111840,,2018-05-31 10:37:52 PM
    
    (Approx within 1 hour of median reported time).

--- translation/download_data.sh
+++ translation/download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2014.en -O tensorflow/newstest2014.en
-wget https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest2014.de -O tensorflow/newstest2014.de
+wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.en -O tensorflow/newstest2014.en
+wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.de -O tensorflow/newstest2014.de
 
 python3 data_download.py --raw_dir raw_data
