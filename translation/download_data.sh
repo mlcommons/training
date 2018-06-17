1b49cf2b8885099baf4ac0d825be5b46b1f3b4f9
tremblerz <abhishek.s14@iiits.in>
2018-06-17 00:00:11 -0700

    Move tensorflow code inwards, add .gitignore files

--- translation/download_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.en -O tensorflow/newstest2014.en
-wget https://raw.githubusercontent.com/tensorflow/models/master/official/transformer/test_data/newstest2014.de -O tensorflow/newstest2014.de
-
-python3 data_download.py --raw_dir raw_data
