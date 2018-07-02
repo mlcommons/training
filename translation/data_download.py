ba605ea4b953402e11a288656fd7af58d9c098ab
tremblerz <abhishek.s14@iiits.in>
2018-07-02 12:21:37 -0700

    remove dead code from data_download

--- translation/data_download.py
+++ translation/data_download.py
@@ -30,12 +30,6 @@ import tensorflow as tf
 import urllib.request
 
 # Data sources for training/evaluating the transformer translation model.
-# If any of the training sources are changed, then either:
-#   1) use the flag `--search` to find the best min count or
-#   2) update the _TRAIN_DATA_MIN_COUNT constant.
-# min_count is the minimum number of times a token must appear in the data
-# before it is added to the vocabulary. "Best min count" refers to the value
-# that generates a vocabulary set that is closest in size to _TARGET_VOCAB_SIZE.
 _TRAIN_DATA_SOURCES = [
     {
         "url": "http://data.statmt.org/wmt17/translation-task/"
@@ -54,8 +48,6 @@ _TRAIN_DATA_SOURCES = [
         "target": "europarl-v7.de-en.de",
     },
 ]
-# Use pre-defined minimum count to generate subtoken vocabulary.
-_TRAIN_DATA_MIN_COUNT = 6
 
 _EVAL_DATA_SOURCES = [
     {
@@ -64,21 +56,6 @@ _EVAL_DATA_SOURCES = [
         "target": "newstest2013.de",
     }
 ]
-
-# Vocabulary constants
-_TARGET_VOCAB_SIZE = 32768  # Number of subtokens in the vocabulary list.
-_TARGET_THRESHOLD = 327  # Accept vocabulary if size is within this threshold
-_VOCAB_FILE = "vocab.ende.%d" % _TARGET_VOCAB_SIZE
-
-# Strings to inclue in the generated files.
-_PREFIX = "wmt32k"
-_TRAIN_TAG = "train"
-_EVAL_TAG = "dev"  # Following WMT and Tensor2Tensor conventions, in which the
-                   # evaluation datasets are tagged as "dev" for development.
-
-# Number of files to split train and evaluation data
-_TRAIN_SHARDS = 100
-_EVAL_SHARDS = 1
 
 
 def find_file(path, filename, max_depth=5):
@@ -206,153 +183,6 @@ def download_and_extract(path, url, input_filename, target_filename):
                 (url, path))
 
 
-def txt_line_iterator(path):
-  """Iterate through lines of file."""
-  with tf.gfile.Open(path) as f:
-    for line in f:
-      yield line.strip()
-
-
-def compile_files(raw_dir, raw_files, tag):
-  """Compile raw files into a single file for each language.
-
-  Args:
-    raw_dir: Directory containing downloaded raw files.
-    raw_files: Dict containing filenames of input and target data.
-      {"inputs": list of files containing data in input language
-       "targets": list of files containing corresponding data in target language
-      }
-    tag: String to append to the compiled filename.
-
-  Returns:
-    Full path of compiled input and target files.
-  """
-  tf.logging.info("Compiling files with tag %s." % tag)
-  filename = "%s-%s" % (_PREFIX, tag)
-  input_compiled_file = os.path.join(raw_dir, filename + ".lang1")
-  target_compiled_file = os.path.join(raw_dir, filename + ".lang2")
-
-  with tf.gfile.Open(input_compiled_file, mode="w") as input_writer:
-    with tf.gfile.Open(target_compiled_file, mode="w") as target_writer:
-      for i in range(len(raw_files["inputs"])):
-        input_file = raw_files["inputs"][i]
-        target_file = raw_files["targets"][i]
-
-        tf.logging.info("Reading files %s and %s." % (input_file, target_file))
-        write_file(input_writer, input_file)
-        write_file(target_writer, target_file)
-  return input_compiled_file, target_compiled_file
-
-
-def write_file(writer, filename):
-  """Write all of lines from file using the writer."""
-  for line in txt_line_iterator(filename):
-    writer.write(line)
-    writer.write("\n")
-
-
-###############################################################################
-# Data preprocessing
-###############################################################################
-def encode_and_save_files(
-    subtokenizer, data_dir, raw_files, tag, total_shards):
-  """Save data from files as encoded Examples in TFrecord format.
-
-  Args:
-    subtokenizer: Subtokenizer object that will be used to encode the strings.
-    data_dir: The directory in which to write the examples
-    raw_files: A tuple of (input, target) data files. Each line in the input and
-      the corresponding line in target file will be saved in a tf.Example.
-    tag: String that will be added onto the file names.
-    total_shards: Number of files to divide the data into.
-
-  Returns:
-    List of all files produced.
-  """
-  # Create a file for each shard.
-  filepaths = [shard_filename(data_dir, tag, n + 1, total_shards)
-               for n in range(total_shards)]
-
-  if all_exist(filepaths):
-    tf.logging.info("Files with tag %s already exist." % tag)
-    return filepaths
-
-  tf.logging.info("Saving files with tag %s." % tag)
-  input_file = raw_files[0]
-  target_file = raw_files[1]
-
-  # Write examples to each shard in round robin order.
-  tmp_filepaths = [fname + ".incomplete" for fname in filepaths]
-  writers = [tf.python_io.TFRecordWriter(fname) for fname in tmp_filepaths]
-  counter, shard = 0, 0
-  for counter, (input_line, target_line) in enumerate(zip(
-      txt_line_iterator(input_file), txt_line_iterator(target_file))):
-    if counter > 0 and counter % 100000 == 0:
-      tf.logging.info("\tSaving case %d." % counter)
-    example = dict_to_example(
-        {"inputs": subtokenizer.encode(input_line, add_eos=True),
-         "targets": subtokenizer.encode(target_line, add_eos=True)})
-    writers[shard].write(example.SerializeToString())
-    shard = (shard + 1) % total_shards
-  for writer in writers:
-    writer.close()
-
-  for tmp_name, final_name in zip(tmp_filepaths, filepaths):
-    tf.gfile.Rename(tmp_name, final_name)
-
-  tf.logging.info("Saved %d Examples", counter)
-  return filepaths
-
-
-def shard_filename(path, tag, shard_num, total_shards):
-  """Create filename for data shard."""
-  return os.path.join(
-      path, "%s-%s-%.5d-of-%.5d" % (_PREFIX, tag, shard_num, total_shards))
-
-
-def shuffle_records(fname):
-  """Shuffle records in a single file."""
-  tf.logging.info("Shuffling records in file %s" % fname)
-
-  # Rename file prior to shuffling
-  tmp_fname = fname + ".unshuffled"
-  tf.gfile.Rename(fname, tmp_fname)
-
-  reader = tf.python_io.tf_record_iterator(tmp_fname)
-  records = []
-  for record in reader:
-    records.append(record)
-    if len(records) % 100000 == 0:
-      tf.logging.info("\tRead: %d", len(records))
-
-  random.shuffle(records)
-
-  # Write shuffled records to original file name
-  with tf.python_io.TFRecordWriter(fname) as w:
-    for count, record in enumerate(records):
-      w.write(record)
-      if count > 0 and count % 100000 == 0:
-        tf.logging.info("\tWriting record: %d" % count)
-
-  tf.gfile.Remove(tmp_fname)
-
-
-def dict_to_example(dictionary):
-  """Converts a dictionary of string->int to a tf.Example."""
-  features = {}
-  for k, v in six.iteritems(dictionary):
-    features[k] = tf.train.Feature(int64_list=tf.train.Int64List(value=v))
-  return tf.train.Example(features=tf.train.Features(feature=features))
-
-
-def all_exist(filepaths):
-  """Returns true if all files in the list exist."""
-  for fname in filepaths:
-    if not tf.gfile.Exists(fname):
-      return False
-  return True
-
-
 def make_dir(path):
   if not tf.gfile.Exists(path):
     tf.logging.info("Creating directory %s" % path)
@@ -382,10 +212,6 @@ if __name__ == "__main__":
       help="[default: %(default)s] Path where the raw data will be downloaded "
            "and extracted.",
       metavar="<RD>")
-  parser.add_argument(
-      "--search", action="store_true",
-      help="If set, use binary search to find the vocabulary set with size"
-           "closest to the target size (%d)." % _TARGET_VOCAB_SIZE)
 
   FLAGS, unparsed = parser.parse_known_args()
   main(sys.argv)
