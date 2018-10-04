1223ca41a72676fbdf57ab4a69bb7ea5f4480d79
Chris Forster <37458626+nvcforster@users.noreply.github.com>
2018-10-04 16:44:18 -0700

    [Transformer] Remove Preprocess Timing (#139)
    * [Translation] Updating file location for concatenation of downloaded files from raw_data to processed_data in order to make raw_data read-only. The motivation for this is to allow a single dataset download that can be placed on a shared filesystem of a cluster and avoid a potential race condition (assuming output folders are independent for each job).
    
    * Updating transformer scripts to exclude preprocessing timing

---
+++ translation/tensorflow/run_preprocessing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+SEED=$1
+
+cd /research/transformer
+
+# TODO: Add SEED to process_data.py since this uses a random generator (future PR)
+export PYTHONPATH=/research/transformer/transformer:${PYTHONPATH}
+python3 process_data.py --raw_dir /raw_data/ --data_dir processed_data
