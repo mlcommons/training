a3e807d5c4186077ae430feb91eea28bf9fc55a7
Taylor Robie <taylorrobie@google.com>
2018-10-30 14:22:02 -0700

    Several bug fixes to prevent crashes. (#5)
    * fix crash when there are no log lines
    
    * fix crash when eval_target is a dict
    
    don't include idea files

--- mlp_compliance.py
+++ mlp_compliance.py
@@ -80,14 +80,19 @@ def check_log(loglines):
     return True
 
 
+def get_value(x):
+  if isinstance(x, dict):
+    return x.get("value")
+  return x
+
+
 def get_model_accuracy(loglines):
-    eval_target = {i.value for i in loglines if i.tag == 'eval_target'}
+    eval_target = {get_value(i.value) for i in loglines if i.tag == 'eval_target'}
     if len(eval_target) == 1:
         eval_target = eval_target.pop()
     else:
         print("Failed to extract eval target.")
         eval_target = None
-    print(eval_target)
 
     values = [0]
     for i in loglines:
@@ -131,6 +136,10 @@ def check_loglines_l2(loglines):
 
     general_l2_file = os.path.join(CONFIG_DIR, 'v0.5.0_level2.yaml')
     l2_check = configured_checks(loglines,  general_l2_file)
+
+    if not loglines:
+      print("No log lines detected.")
+      return False
 
     benchmark = loglines[0].benchmark
     benchmark_file = os.path.join(CONFIG_DIR, 'v0.5.0_l2_{}.yaml'.format(benchmark))
