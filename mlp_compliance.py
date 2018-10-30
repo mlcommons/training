e76528443ecc26124b6d62a8b017c9e3050c26c6
Taylor Robie <taylorrobie@google.com>
2018-10-30 11:35:54 -0700

    report accuracy and target from log parser (#4)

--- mlp_compliance.py
+++ mlp_compliance.py
@@ -80,6 +80,26 @@ def check_log(loglines):
     return True
 
 
+def get_model_accuracy(loglines):
+    eval_target = {i.value for i in loglines if i.tag == 'eval_target'}
+    if len(eval_target) == 1:
+        eval_target = eval_target.pop()
+    else:
+        print("Failed to extract eval target.")
+        eval_target = None
+    print(eval_target)
+
+    values = [0]
+    for i in loglines:
+        if i.tag != 'eval_accuracy':
+            continue
+        try:
+            values.append(i.value["value"])
+        except:
+            pass
+    return max(values), eval_target
+
+
 def l1_check_file(filename):
     loglines, errors = mlp_parser.parse_file(filename)
 
@@ -91,17 +111,19 @@ def l1_check_file(filename):
         print()
         print('FAILED: log lines had parsing errors.')
         print('FAILED: Logs are NOT L1 compliant.')
-        return False, 0, 0
+        return False, 0, 0, None
 
     check_ok = check_log(loglines)
 
     if not check_ok:
         print('FAILED: Logs are NOT L1 compliant.')
-        return False, 0, 0
+        return False, 0, 0, None
 
     dt = mlp_common_checks.check_clock(loglines)
+    accuracy, eval_target = get_model_accuracy(loglines)
+
     print('SUCCESS: logs are L1 compliant.')
-    return True, dt, 0
+    return True, dt, accuracy, eval_target
 
 
 def check_loglines_l2(loglines):
@@ -131,17 +153,19 @@ def l2_check_file(filename):
         print()
         print('FAILED: log lines had parsing errors.')
         print('FAILED: Logs are NOT L2 compliant.')
-        return False, 0, 0
+        return False, 0, 0, None
 
     check_ok = check_loglines_l2(loglines)
 
     if not check_ok:
         print('FAILED: Logs are NOT L2 compliant.')
-        return False, 0, 0
+        return False, 0, 0, None
 
     dt = mlp_common_checks.check_clock(loglines)
+    accuracy, eval_target = get_model_accuracy(loglines)
+
     print('SUCCESS: logs are L2 compliant.')
-    return True, dt, 0
+    return True, dt, accuracy, eval_target
 
 
 def main():
@@ -155,18 +179,27 @@ def main():
     args = parser.parse_args()
     print(args)
 
+    lj_len = 20
     if args.level == 1:
-      status, dt, qual = l1_check_file(args.filename)
+      status, dt, qual, target = l1_check_file(args.filename)
       if status:
-          print('Measured time: ', dt)
+          print('Measured time: '.ljust(lj_len), dt)
+          if qual:
+              print('Best Eval Accuracy: '.ljust(lj_len), qual)
+          if target:
+              print('Target: '.ljust(lj_len), target)
           sys.exit(0)
       else:
           sys.exit(1)
 
     if args.level == 2:
-      status, dt, qual = l2_check_file(args.filename)
+      status, dt, qual, target = l2_check_file(args.filename)
       if status:
-          print('Measured time: ', dt)
+          print('Measured time: '.ljust(lj_len), dt)
+          if qual:
+              print('Best Eval Accuracy: '.ljust(lj_len), qual)
+          if target:
+              print('Target: '.ljust(lj_len), target)
           sys.exit(0)
       else:
           sys.exit(1)
