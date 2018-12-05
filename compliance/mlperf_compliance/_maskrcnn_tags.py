7788f8d7c13dbacaf4eb33e3fecddef6d8220257
sharath-ts <39533860+sharath-ts@users.noreply.github.com>
2018-12-04 19:23:47 -0800

    Adding to MaskRCNN tagset (#206)
    * Adding to mask rcnn tagset
    
    * Adding to mask rcnn tagset
    
    * adding to maskrcnn tagset

--- compliance/mlperf_compliance/_maskrcnn_tags.py
+++ compliance/mlperf_compliance/_maskrcnn_tags.py
@@ -30,6 +30,9 @@ RPN_PRE_NMS_TOP_N_TEST = "rpn_pre_nms_top_n_test"
 RPN_POST_NMS_TOP_N_TRAIN = "rpn_post_nms_top_n_train"
 RPN_POST_NMS_TOP_N_TEST = "rpn_post_nms_top_n_test"
 
+#Global batch size during training
+GLOBAL_BATCH_SIZE = "global_batch_size"
+
 # Batch size during eval
 BATCH_SIZE_TEST = "batch_size_test"
 
