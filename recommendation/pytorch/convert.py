36b3e14e02a91e8e1d44fd2be81d518f9eeebf53
pkanwar <pkanwar@google.com>
2019-04-01 16:26:10 -0700

    integrate alias table

--- recommendation/pytorch/convert.py
+++ recommendation/pytorch/convert.py
@@ -1,119 +1,86 @@
-import os
 from argparse import ArgumentParser
-from collections import defaultdict
-
-import numpy as np
 import pandas as pd
-from tqdm import tqdm
+import numpy as np
+import torch
+from datetime import datetime
+import numpy_indexed as npi
+import pickle
+from alias_generator import process_data
+from alias_generator import profile_sampler
 
-from load import implicit_load
-
-from mlperf_compliance import mlperf_log
-
-
-MIN_RATINGS = 20
-
-
-USER_COLUMN = 'user_id'
-ITEM_COLUMN = 'item_id'
-
-
-TRAIN_RATINGS_FILENAME = 'train-ratings.csv'
-TEST_RATINGS_FILENAME = 'test-ratings.csv'
-TEST_NEG_FILENAME = 'test-negative.csv'
-
+_PREFIX = "16x32_"
 
 def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument('path', type=str,
-                        help='Path to reviews CSV file from MovieLens')
-    parser.add_argument('output', type=str,
-                        help='Output directory for train and test CSV files')
-    parser.add_argument('-n', '--negatives', type=int, default=999,
-                        help='Number of negative samples for each positive'
-                             'test example')
-    parser.add_argument('-s', '--seed', type=int, default=0,
-                        help='Random seed to reproduce same negative samples')
+    parser = ArgumentParser(description="Train a Nerual Collaborative"
+                                        " Filtering model converter")
+    parser.add_argument('data', type=str,
+                        help='path to test and training data files')
+    parser.add_argument('--valid-negative', type=int, default=999,
+                        help='Number of negative samples for each positive test example')
+    parser.add_argument('--user_scaling', default=16, type=int)
+    parser.add_argument('--item_scaling', default=32, type=int)
+
     return parser.parse_args()
 
 
+def generate_negatives(sampler, num_negatives, test_ratings):
+    users = test_ratings[:,0].numpy()
+    neg_users_items = np.empty([num_negatives], object)
+    for i in range(num_negatives):
+      negatives = np.array([users, sampler.sample_negatives(users)])
+      neg_users_items[i] = negatives.transpose()
+    return neg_users_items;
+
 def main():
     args = parse_args()
-    np.random.seed(args.seed)
 
-    print("Loading raw data from {}".format(args.path))
-    df = implicit_load(args.path, sort=False)
+    train_ratings = torch.LongTensor()
+    test_chunk_size = [0] * args.user_scaling
+    for chunk in range(args.user_scaling):
+        train_ratings = torch.cat((train_ratings,
+            torch.from_numpy(np.load(args.data + '/trainx'
+                + str(args.user_scaling) + 'x' + str(args.item_scaling)
+                + '_' + str(chunk) + '.npz', encoding='bytes')['arr_0'])))
+        test_ratings_chunk = torch.from_numpy(np.load(args.data + '/testx'
+                + str(args.user_scaling) + 'x' + str(args.item_scaling)
+                + '_' + str(chunk) + '.npz', encoding='bytes')['arr_0'])
+        test_chunk_size[chunk] = test_ratings_chunk.shape[0]
 
-    print("Filtering out users with less than {} ratings".format(MIN_RATINGS))
-    grouped = df.groupby(USER_COLUMN)
-    mlperf_log.ncf_print(key=mlperf_log.PREPROC_HP_MIN_RATINGS, value=MIN_RATINGS)
-    df = grouped.filter(lambda x: len(x) >= MIN_RATINGS)
+    nb_maxs = torch.max(train_ratings, 0)[0]
+    nb_users = nb_maxs[0].item()+1
+    nb_items = nb_maxs[1].item()+1
+    print(datetime.now(), 'number of users, items...', nb_users, nb_items)
+    train_input = npi.group_by(train_ratings[:, 0]).split(train_ratings[:, 1])
 
-    print("Mapping original user and item IDs to new sequential IDs")
-    original_users = df[USER_COLUMN].unique()
-    original_items = df[ITEM_COLUMN].unique()
+    def iter_fn():
+      for _, items in enumerate(train_input):
+         yield items
 
-    user_map = {user: index for index, user in enumerate(original_users)}
-    item_map = {item: index for index, item in enumerate(original_items)}
+    sampler, pos_users, pos_items = process_data(num_items=nb_items, min_items_per_user=1, iter_fn=iter_fn)
 
-    df[USER_COLUMN] = df[USER_COLUMN].apply(lambda user: user_map[user])
-    df[ITEM_COLUMN] = df[ITEM_COLUMN].apply(lambda item: item_map[item])
+    sampler_cache = _PREFIX + "cached_sampler.pkl"
+    with open(sampler_cache, "wb") as f:
+        pickle.dump([sampler, pos_users, pos_items], f, pickle.HIGHEST_PROTOCOL)
 
-    assert df[USER_COLUMN].max() == len(original_users) - 1
-    assert df[ITEM_COLUMN].max() == len(original_items) - 1
+    print(datetime.now(), 'Generating negative test samples...')
 
-    print("Creating list of items for each user")
-    # Need to sort before popping to get last item
-    df.sort_values(by='timestamp', inplace=True)
-    all_ratings = set(zip(df[USER_COLUMN], df[ITEM_COLUMN]))
-    user_to_items = defaultdict(list)
-    for row in tqdm(df.itertuples(), desc='Ratings', total=len(df)):
-        user_to_items[getattr(row, USER_COLUMN)].append(getattr(row, ITEM_COLUMN))  # noqa: E501
+    test_negatives = [torch.LongTensor()] * args.user_scaling
+    test_user_offset = 0
+    for chunk in range(args.user_scaling):
+        test_negatives[chunk] = generate_negatives(
+                sampler,
+                args.valid_negative,
+                test_ratings_chunk)
 
-    test_ratings = []
-    test_negs = []
-    all_items = set(range(len(original_items)))
+        file_name = (args.data + '/test_negx' + str(args.user_scaling) + 'x'
+                + str(args.item_scaling) + '_' + str(chunk) + '.npz')
 
-    print("Generating {} negative samples for each user"
-          .format(args.negatives))
-    mlperf_log.ncf_print(key=mlperf_log.PREPROC_HP_NUM_EVAL, value=args.negatives)
-
-    # The default of np.random.choice is replace=True
-    mlperf_log.ncf_print(key=mlperf_log.PREPROC_HP_SAMPLE_EVAL_REPLACEMENT, value=True)
-
-    #===========================================================================
-    #== First random operation triggers the clock start. =======================
-    #===========================================================================
-    mlperf_log.ncf_print(key=mlperf_log.RUN_START)
-    mlperf_log.ncf_print(key=mlperf_log.INPUT_STEP_EVAL_NEG_GEN)
-
-    for user in tqdm(range(len(original_users)), desc='Users', total=len(original_users)):  # noqa: E501
-        test_item = user_to_items[user].pop()
-
-        all_ratings.remove((user, test_item))
-        all_negs = all_items - set(user_to_items[user])
-        all_negs = sorted(list(all_negs))  # determinism
-
-        test_ratings.append((user, test_item))
-        test_negs.append(list(np.random.choice(all_negs, args.negatives)))
-
-    print("Saving train and test CSV files to {}".format(args.output))
-    df_train_ratings = pd.DataFrame(list(all_ratings))
-    df_train_ratings['fake_rating'] = 1
-    df_train_ratings.to_csv(os.path.join(args.output, TRAIN_RATINGS_FILENAME),
-                            index=False, header=False, sep='\t')
-
-    mlperf_log.ncf_print(key=mlperf_log.INPUT_SIZE, value=len(df_train_ratings))
-
-    df_test_ratings = pd.DataFrame(test_ratings)
-    df_test_ratings['fake_rating'] = 1
-    df_test_ratings.to_csv(os.path.join(args.output, TEST_RATINGS_FILENAME),
-                           index=False, header=False, sep='\t')
-
-    df_test_negs = pd.DataFrame(test_negs)
-    df_test_negs.to_csv(os.path.join(args.output, TEST_NEG_FILENAME),
-                        index=False, header=False, sep='\t')
+        print("test negatives:", len(test_negatives[chunk]))
+        np.savez_compressed(file_name, test_negatives[chunk])
+        print(datetime.now(), 'Chunk ', chunk, '/', args.user_scaling, 'saved.')
+        test_user_offset += test_chunk_size[chunk]
 
 
 if __name__ == '__main__':
     main()
+
