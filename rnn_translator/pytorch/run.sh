09a7ab145d2dc9d3540ac8ce16795808f1d55e0a
Marek Wawrzos <mwawrzos@nvidia.com>
2019-10-17 14:50:59 +0200

    multi GPU and gradient accumulation enabled in launch scripts for GNMT

--- rnn_translator/pytorch/run.sh
+++ rnn_translator/pytorch/run.sh
@@ -4,11 +4,51 @@ set -e
 
 DATASET_DIR='/data'
 
-SEED=${1:-"1"}
-TARGET=${2:-"24.00"}
+usage() {
+  echo \
+    Usage: run.sh \
+    [--random_seed RANDOM_SEED] \
+    [--bleu_threshold TARGET_BLEU_SCORE] \
+    [--num_gpus NUM_GPUS]
+}
+
+SEED=1
+TARGET=24.00
+MULTI_GPU_WRAPPER=
+
+MULTI_GPU_COMMAND='-m torch.distributed.launch --nproc_per_node'
+
+while [ "$1" != "" ]; do
+  case $1 in
+    --random_seed )
+      shift
+      if [[ $1 == --* ]] ; then usage; exit 1; fi
+      if [ "$1" != "" ] ; then SEED=$1; fi
+      ;;
+    --bleu_threshold )
+      shift
+      if [[ $1 == --* ]] ; then usage; exit 1; fi
+      if [ "$1" != "" ]; then TARGET=$1; fi
+      ;;
+    --num_gpus )
+      shift
+      if [[ $1 == --* ]] ; then usage; exit 1; fi
+      if [ "$1" != "" ] && [ "$1" != "1" ] ; then MULTI_GPU_WRAPPER="$MULTI_GPU_COMMAND $1"; fi
+      ;;
+    -h | --help )
+      usage
+      exit
+      ;;
+    * )
+      usage
+      exit 1
+  esac
+  shift
+done
 
 # run training
-python3 train.py \
+python3 $MULTI_GPU_WRAPPER train.py \
   --dataset-dir ${DATASET_DIR} \
   --seed $SEED \
+  --train-global-batch-size 1024 \
   --target-bleu $TARGET
--- rnn_translator/pytorch/run_and_time.sh
+++ rnn_translator/pytorch/run_and_time.sh
@@ -1,22 +1,61 @@
 #!/bin/bash
 
-# runs benchmark and reports time to convergence
-# to use the script:
-#   run_and_time.sh
-
 set -e
+
+DEFAULT_SEED=1
+DEFAULT_TARGET_UNCASED_BLEU_SCORE=24.00
+
+usage() {
+  echo \
+    Usage: run_and_time.sh \
+    [--random_seed RANDOM_SEED] \
+    [--bleu_threshold TARGET_BLEU_SCORE] \
+    [--num_gpus NUM_GPUS]
+}
+
+seed=${DEFAULT_SEED}
+random_seed_arg="--random_seed ${DEFAULT_SEED}"
+bleu_threshold_arg="--bleu_threshold ${DEFAULT_TARGET_UNCASED_BLEU_SCORE}"
+num_gpus_arg=
+
+while [ "$1" != "" ]; do
+  case $1 in
+    --random_seed )
+      shift
+      if [[ $1 == --* ]] ; then usage; exit 1; fi
+      if [ "$1" != "" ] ; then random_seed_arg="--random_seed $1"; seed=$1; fi
+      ;;
+    --bleu_threshold )
+      shift
+      if [[ $1 == --* ]] ; then usage; exit 1; fi
+      if [ "$1" != "" ]; then bleu_threshold_arg="--bleu_threshold $1"; fi
+      ;;
+    --num_gpus )
+      shift
+      if [[ $1 == --* ]] ; then usage; exit 1; fi
+      if [ "$1" != "" ]; then num_gpus_arg="--num_gpus $1"; fi
+      ;;
+    -h | --help )
+      usage
+      exit
+      ;;
+    * )
+      usage
+      exit 1
+  esac
+  shift
+done
 
 # start timing
 start=$(date +%s)
 start_fmt=$(date +%Y-%m-%d\ %r)
 echo "STARTING TIMING RUN AT $start_fmt"
 
-# run benchmark
-seed=${1:-"1"}
-target=24.00
-
 echo "running benchmark"
-./run.sh $seed $target
+./run.sh \
+  ${random_seed_arg} \
+  ${bleu_threshold_arg} \
+  ${num_gpus_arg}
 
 sleep 3
 ret_code=$?; if [[ $ret_code != 0 ]]; then exit $ret_code; fi
