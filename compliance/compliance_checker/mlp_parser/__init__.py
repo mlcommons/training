378b6da0094f99427afd397560b875669a34d917
Marek Wawrzos <mwawrzos@nvidia.com>
2020-03-27 20:55:38 +0100

    compliance checker 0.7.0 ruleset

--- compliance/compliance_checker/mlp_parser/__init__.py
+++ compliance/compliance_checker/mlp_parser/__init__.py
@@ -1,7 +1,10 @@
 from .ruleset_060 import parse_file as parse_file_060
+from .ruleset_070 import parse_file as parse_file_070
 
 def parse_file(filename, ruleset='0.6.0'):
     if ruleset == '0.6.0':
         return parse_file_060(filename)
+    elif ruleset == '0.7.0':
+        return parse_file_070(filename)
     else:
         raise Exception(f'Ruleset "{ruleset}" is not supported')
