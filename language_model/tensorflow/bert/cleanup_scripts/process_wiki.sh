0f2257657494faace172e4967ed253f4beeac524
Yuechao Pan <panyuechao@gmail.com>
2021-02-25 12:42:39 +0000

    [BERT] redirect seperated dataset changes to TF1

--- language_model/tensorflow/bert/cleanup_scripts/process_wiki.sh
+++ language_model/tensorflow/bert/cleanup_scripts/process_wiki.sh
@@ -10,21 +10,24 @@ inputs=$1
 pip install nltk
 
 # Remove doc tag and title
-python ./cleanup_file.py --data=$inputs --output_suffix='.1'
+# python ./cleanup_file.py --data=$inputs --output_suffix='.1'
 
 # Further clean up files
-for f in ${inputs}; do
-  ./clean.sh ${f}.1 ${f}.2
-done
+# for f in ${inputs}; do
+#   ./clean.sh ${f}.1 ${f}.2
+# done
 
 # Sentence segmentation
-python ./do_sentence_segmentation.py --data=$inputs --input_suffix='.2' --output_suffix='.3'
+# python ./do_sentence_segmentation.py --data=$inputs --input_suffix='.2' --output_suffix='.3'
 
 mkdir -p ./results
 
+# Train/Eval seperation
+python ./seperate_test_set.py --data=$inputs --input_suffix='.3' --output_suffix='.4' --num_test_articles=10000 --test_output='./results/eval'
+
 ## Choose file size method or number of packages by uncommenting only one of the following do_gather options
 # Gather into fixed size packages
-python ./do_gather.py --data=$inputs --input_suffix='.3' --block_size=26.92 --out_dir='./results'
+python ./do_gather.py --data=$inputs --input_suffix='.4' --block_size=26.92 --out_dir='./results'
 
 # Gather into fixed number of packages
 #NUM_PACKAGES=512
