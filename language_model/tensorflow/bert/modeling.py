[BERT] fixes and gradient accumulation for TF1 (#451)
* [BERT] fixing input pipeline and layer norm namescope

* [BERT] input preprocessing for tf2 model, seperating training and eval sets

* [BERT] small corrections to README.md

* Shell script to download and extract, V0.7 README

* Minor fix to README

* [BERT] small fixes to tpu library imports

* [BERT] Gradient accumulation for TF1

* Fixing git SHA and nltk versions

* [BERT] redirect seperated dataset changes to TF1

* [BERT] revert back run commands

* [BERT] uncommand process_wiki.sh

* [BERT] remove 3M starting eval requirement and update target accuracy to 0.720

* [BERT] delete unused files

* [BERT] add MLPerf logging

* [BERT] small corrections

* [BERT] update eval frequency

* [BERT] provides dataset after preprocessing, and move the related details to dataset.md

* [BERT] small corrections

* [BERT] update HPs for BS24 on V100x8, and add BS8k running steps on TPUs

* [BERT] update README to give more details about how to use eval

* [BERT] Readme update for eval

* Revert "Merge pull request #1 from aarti-cerebras/v0.7_readme"

This reverts commit 9974ac9d6d6bf0b3ceaf22d4d86c1f5f25ba26e4, reversing
changes made to 563be596bd7f5c38db696e9c34ef29bd477462f7.

* Revert "Revert "Merge pull request #1 from aarti-cerebras/v0.7_readme""

This reverts commit f38ef1626517ae74a3be2895861ee17ee0d699c9.

* [BERT] add clip_by_global_norm_after_gradient_allreduce option

* [BERT] sample script to run offline eval

Co-authored-by: Aarti Ghatkesar <aarti@cerebras.net>

language_model/tensorflow/bert/modeling.py
@@ -363,7 +363,8 @@ def layer_norm(input_tensor, name=None):
   # return contrib_layers.layer_norm(
   #     inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
   return tf.keras.layers.LayerNormalization(
-          name=name, axis=-1, dtype=tf.float32, epsilon=1e-12)(input_tensor)
+          name="LayerNorm" if not name else name, axis=-1,
+          dtype=tf.float32, epsilon=1e-12)(input_tensor)
   
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):

language_model/tensorflow/bert/optimization.py
@@ -5,15 +5,37 @@ from __future__ import division
 from __future__ import print_function
 
 import re
+import absl
 import tensorflow.compat.v1 as tf
 
 # from tensorflow.contrib import tpu as contrib_tpu
 
 import lamb_optimizer_v1 as lamb_optimizer
+import deferred_grad_optimizer
+
+flags = absl.flags
+
+FLAGS = flags.FLAGS
+flags.DEFINE_float("lamb_weight_decay_rate", 0.01, "Lamb weight decay")
+flags.DEFINE_float("lamb_beta_1", 0.9, "Lamb beta1")
+flags.DEFINE_float("lamb_beta_2", 0.999, "Lamb beta2")
+flags.DEFINE_float("log_epsilon", -6, "Lamb beta2")
+flags.DEFINE_bool(
+    "clip_by_global_norm_after_gradient_allreduce",
+    False,
+    "Whether to apply clip_by_global_norm to gradients after gradient "
+    "reductions across multiple replicas.")
 
 
-def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu,
-                     optimizer_name='adamw', poly_power=1.0, start_warmup_step=0):
+def create_optimizer(loss,
+                     init_lr,
+                     num_train_steps,
+                     num_warmup_steps,
+                     use_tpu,
+                     optimizer_name="adamw",
+                     poly_power=1.0,
+                     start_warmup_step=0,
+                     steps_per_update=1):
   """Creates an optimizer training op."""
   global_step = tf.train.get_or_create_global_step()
 
@@ -57,7 +79,7 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu,
   # is 3e-5, 4e-5 or 5e-5. For LAMB, the users can use 3e-4, 4e-4,or 5e-4 for a
   # batch size of 64 in the finetune.
   if optimizer_name == "adamw":
-    tf.logging.info('using adamw')
+    tf.logging.info("using adamw")
     optimizer = AdamWeightDecayOptimizer(
         learning_rate=learning_rate,
         weight_decay_rate=0.01,
@@ -66,25 +88,33 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu,
         epsilon=1e-6,
         exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
   elif optimizer_name == "lamb":
-    tf.logging.info('using lamb')
+    tf.logging.info("using lamb")
     optimizer = lamb_optimizer.LAMBOptimizer(
         learning_rate=learning_rate,
-        weight_decay_rate=0.01,
-        beta_1=0.9,
-        beta_2=0.999,
-        epsilon=1e-6,
-        exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
+        weight_decay_rate=FLAGS.lamb_weight_decay_rate,
+        beta_1=FLAGS.lamb_beta_1,
+        beta_2=FLAGS.lamb_beta_2,
+        epsilon=10**FLAGS.log_epsilon,
+        exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"],
+        clip_by_global_norm_after_gradient_allreduce=FLAGS
+        .clip_by_global_norm_after_gradient_allreduce)
   else:
     raise ValueError("Not supported optimizer: ", optimizer_name)
 
-  # if use_tpu:
-  #   optimizer = contrib_tpu.CrossShardOptimizer(optimizer)
+  if steps_per_update > 1:
+    tf.logging.info("applying gradient aggregation %d " % steps_per_update)
+    optimizer = deferred_grad_optimizer.GradientAggregationOptimizer(
+        optimizer, steps_per_update, use_tpu=use_tpu)
+
+  if use_tpu:
+    optimizer = tf.tpu.CrossShardOptimizer(optimizer)
 
   tvars = tf.trainable_variables()
   grads = tf.gradients(loss, tvars)
 
-  # This is how the model was pre-trained.
-  (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
+  if not FLAGS.clip_by_global_norm_after_gradient_allreduce:
+    # This is how the model was pre-trained.
+    (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
 
   train_op = optimizer.apply_gradients(
       zip(grads, tvars), global_step=global_step)