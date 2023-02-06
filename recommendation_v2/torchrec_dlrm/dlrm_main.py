ad55e9aee9d1d629d50e213a80472ce344902a29
Jan Lasek <janek.lasek@gmail.com>
2023-02-06 16:25:17 +0100

    Align optimizer parameters for embeddings and dense layers

--- recommendation_v2/torchrec_dlrm/dlrm_main.py
+++ recommendation_v2/torchrec_dlrm/dlrm_main.py
@@ -788,10 +788,17 @@ def main(argv: List[str]) -> None:
     # the optimizer update will be applied in the backward pass, in this case through a fused op.
     # TorchRec will use the FBGEMM implementation of EXACT_ADAGRAD. For GPU devices, a fused CUDA kernel is invoked. For CPU, FBGEMM_GPU invokes CPU kernels
     # https://github.com/pytorch/FBGEMM/blob/2cb8b0dff3e67f9a009c4299defbd6b99cc12b8f/fbgemm_gpu/fbgemm_gpu/split_table_batched_embeddings_ops.py#L676-L678
+
+    # Note that lr_decay and initial_accumulator_value for Adagrad cannot be specified below (FBGEMM version 0.3.2).
+    # This equivalently means that both parameters are hardcoded to zero.
     apply_optimizer_in_backward(
         embedding_optimizer,
         train_model.model.sparse_arch.parameters(),
-        {"lr": args.learning_rate},
+        {
+            "lr": args.learning_rate,
+            "weight_decay": WEIGHT_DECAY,
+            "eps": ADAGRAD_EPS,
+        },
     )
     planner = EmbeddingShardingPlanner(
         topology=Topology(
