b51781aca0e112acb71c9df78b5a8561db86dc7d
David Jurado <david.jurado@factored.ai>
2023-09-14 10:12:31 -0500

    Fix create pretraining data script

--- language_model/tensorflow/bert/cleanup_scripts/download_and_uncompress.sh
+++ language_model/tensorflow/bert/cleanup_scripts/download_and_uncompress.sh
@@ -35,6 +35,8 @@ gdown https://drive.google.com/uc?id=14_A6gQ0NJ7Pay1X0xFq9rCKUuFJcKLF-
 # enwiki-20200101-pages-articles-multistream.xml.bz2
 gdown https://drive.google.com/uc?id=18K1rrNJ_0lSR9bsLaoP3PkQeSFO-9LE7
 
+echo "Uncompressing, this may take a while..."
+
 bzip2 -dv enwiki-20200101-pages-articles-multistream.xml.bz2
 
 # Download TF-1 checkpoints
