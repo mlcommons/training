e38f693df609aac02c23880d48807ca9efd801d6
Bruno Ferreira <bruno.mgf@gmail.com>
2024-02-17 20:44:57 +0000

    Change dataset download scripts to use Cloudflare buckets directly instead of going through Nextcloud

--- stable_diffusion/scripts/datasets/coco2014-validation-download-prompts.sh
+++ stable_diffusion/scripts/datasets/coco2014-validation-download-prompts.sh
@@ -12,4 +12,8 @@ while [ "$1" != "" ]; do
 done
 
 mkdir -p ${OUTPUT_DIR}
-wget -O ${OUTPUT_DIR}/val2014_30k.tsv -c "https://cloud.mlcommons.org/index.php/s/training_stable_diffusion/download?path=/datasets/coco2014&files=val2014_30k.tsv"
+
+rclone config create mlc-training s3 provider=Cloudflare access_key_id=76ea42eadb867e854061a1806220ee1e secret_access_key=a53625c4d45e3ca8ac0df8a353ea3a41ffc3292aa25259addd8b7dc5a6ce2936 endpoint=c2686074cb2caf5cbaf6d134bdba8b47.r2.cloudflarestorage.com
+
+rclone copy mlc-training:mlcommons-training-wg-public/stable_diffusion/datasets/coco2014/val2014_30k.tsv ${OUTPUT_DIR} -P
+
