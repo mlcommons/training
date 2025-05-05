64c39626e38d3a881aea71b5e7a1b9899265100d
David Jurado <david.jurado@factored.ai>
2025-05-05 16:17:17 -0500

    wip

--- large_language_model_pretraining/nemo/run_demo.sh
+++ large_language_model_pretraining/nemo/run_demo.sh
@@ -3,32 +3,65 @@
 set +x
 set -e
 
-# Start timing
-start=$(date +%s)
-start_fmt=$(date +%Y-%m-%d\ %r)
-echo "STARTING TIMING RUN AT $start_fmt"
+source demo_config.sh
 
-# Set default variables (can be overridden via CLI)
-: "${JOB_DIR:=./workspace/job}"
-: "${PREPROCESSED_PATH:=./dataset/preprocessed}"
-: "${TOKENIZER_PATH:=./tokenizer}"
-: "${MODEL_CKPT:=./checkpoints}"
-: "${CONTINUAL_CKPT:=./checkpoints/continual}"
-: "${USE_CKPT:=0}"
-: "${FROM_HF:=0}"
-: "${SAVE_CKPT:=0}"
+git init
+git add .
+git config --global user.name "test"
+git config --global user.email "test@test.com"
+git commit -m "start"
+
+# Vars without defaults
+#     Slurm settings
+: "${USER:?USER not set}"
+: "${HOST:?HOST not set}"
+: "${ACCOUNT:?ACCOUNT not set}"
+: "${PARTITION:?PARTITION not set}"
+
+#     Job settings
+: "${IMAGE:?IMAGE not set}"
+
+#     Dataset settings
+#: "${PREPROCESSED_PATH:?PREPROCESSED_PATH not set}"
+#: "${TOKENIZER_PATH:?TOKENIZER_PATH not set}"
+
+#     Model settings
+#: "${MODEL_CKPT:?MODEL_CKPT not set}"
+: "${USE_CKPT:?USE_CKPT not set}"
+: "${FROM_HF:?FROM_HF not set}"
+: "${CONTINUAL_CKPT:?CONTINUAL_CKPT not set}"
+
+# Vars with defaults
+#     Slurm settings
+: "${TIME:="04:00:00"}"
+: "${NNODES:=288}"
+: "${GPUS_PER_NODE:=8}"
+: "${DEPENDENCIES:=""}"
+
+#     Job settings
+: "${NEMO_DIR:=""}" # Provide customized NeMo path here
+: "${NEMO_RUN_DIR:=""}" # Provide customized NeMo-Run path here
+: "${TMP_NPY_INDEX:=""}" # Provide temporary NNumpy Index saving directory
 : "${MAX_RETRIES:=0}"
-: "${SIZE:=405b}"
-: "${GBS:=1}"
+
+#     Model settings
+: "${SIZE:="405b"}"
+: "${GBS:=1152}"
 : "${MBS:=1}"
 : "${START_STEPS:=0}"
-: "${MAX_STEPS:=}"
-: "${SEEDS:=42}"
+
+#     Dataloader settings
+: "${MAX_STEPS:=""}"
+
+#     Experiment settings
+: "${SEEDS:=""}"
+IFS=" " read -ra seeds <<< $SEEDS
 : "${NEXP:=1}"
 : "${NPAR:=1}"
-: "${TAG:=default_run}"
-: "${TARGET:=5.6}"
-: "${STEP_TIME_ATOL:=7200}"
+: "${SAVE_CKPT:=0}"
+: "${TAG:=""}"
+: "${TARGET:="5.6"}"
+: "${STEP_TIME_ATOL:="7200"}" # maximum tolerable step time, setting to 2hr by default
 
 # Parse CLI args
 while [ $# -gt 0 ]; do
@@ -58,36 +91,76 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+
+# Run
+
+MOUNTS="${JOB_DIR}:/output,${JOB_DIR}:/mlperf-outputs,${PREPROCESSED_PATH}:/preproc_data,${MODEL_CKPT}:/checkpoint,${TOKENIZER_PATH}:/tokenizer,${CONTINUAL_CKPT}:/continual"
+
+CKPT_OPTION=""
+
 CMD_SUFFIX=""
-[[ "$USE_CKPT" -gt 0 ]] && CMD_SUFFIX+=" --use_ckpt"
-[[ "$FROM_HF" -gt 0 ]] && CMD_SUFFIX+=" --resume_from_hf"
-[[ "$SAVE_CKPT" -gt 0 ]] && CMD_SUFFIX+=" --save_ckpt"
-[[ -n "$MAX_STEPS" ]] && CMD_SUFFIX+=" --max_steps $MAX_STEPS"
-[[ -n "$TAG" ]] && CMD_SUFFIX+=" --tag $TAG"
 
-IFS=" " read -ra seeds <<< "$SEEDS"
+if [ $USE_CKPT -gt 0 ]; then
+    CMD_SUFFIX="${CMD_SUFFIX} --use_ckpt"
+    if [ $FROM_HF -gt 0 ]; then
+        CMD_SUFFIX="${CMD_SUFFIX} --resume_from_hf"
+    fi
+fi
 
-mkdir -p "$JOB_DIR/mlperf-outputs"
+if [ $SAVE_CKPT -gt 0 ]; then 
+    CMD_SUFFIX="${CMD_SUFFIX} --save_ckpt"
+fi
+
+if [ ! $NEMO_DIR = "" ]; then
+    MOUNTS="${MOUNTS},${NEMO_DIR}:/opt/NeMo"
+fi
+
+if [ ! $NEMO_RUN_DIR = "" ]; then
+    MOUNTS="${MOUNTS},${NEMO_RUN_DIR}:/opt/NeMo-Run"
+fi
+
+if [ ! $TMP_NPY_INDEX = "" ]; then
+    MOUNTS="${MOUNTS},${TMP_NPY_INDEX}:/npy_index"
+fi
+
+if [ ! $DEPENDENCIES = "" ]; then 
+    CMD_SUFFIX="${CMD_SUFFIX} --dependencies ${DEPENDENCIES}"
+fi
+
+if [ ! $MAX_STEPS = "" ]; then
+    CMD_SUFFIX="${CMD_SUFFIX} --max_steps ${MAX_STEPS}"
+fi
+
+if [ ! $TAG = "" ]; then
+    CMD_SUFFIX="${CMD_SUFFIX} --tag ${TAG}"
+fi
+
+# Allows MLLogger objects to be constructed locally
+if [ ! -d /mlperf-outputs ]; then mkdir /mlperf-outputs; fi
 
 set -x
 
 python3 pretrain_llama31.py \
-    --job_dir "$JOB_DIR" \
-    --nodes 1 \
-    --gpus_per_node 1 \
-    --size "$SIZE" \
-    --gbs "$GBS" --mbs "$MBS" \
-    --seeds "${seeds[@]}" \
-    --num_exps "$NEXP" \
-    --num_pars "$NPAR" \
-    --initial_ckpt_path "$MODEL_CKPT" \
-    --continual_ckpt_path "$CONTINUAL_CKPT" \
-    --tokenizer_path "$TOKENIZER_PATH" \
-    --target_log_ppl "$TARGET" \
-    --step_time_atol "$STEP_TIME_ATOL" \
-    --ckpt_start_step "$START_STEPS" \
-    --max_retries "$MAX_RETRIES" \
-    $CMD_SUFFIX |& tee "$JOB_DIR/train_console.log"
+--user $USER --host $HOST \
+--job_dir $JOB_DIR \
+--account $ACCOUNT --partition $PARTITION \
+--nodes $NNODES --gpus_per_node $GPUS_PER_NODE \
+--time $TIME \
+--mounts $MOUNTS \
+--image $IMAGE \
+--size $SIZE \
+--gbs $GBS --mbs $MBS \
+--seeds ${seeds[@]} \
+--num_exps $NEXP \
+--num_pars $NPAR \
+--initial_ckpt_path /checkpoint \
+--continual_ckpt_path /continual \
+--tokenizer_path /tokenizer \
+--target_log_ppl $TARGET \
+--step_time_atol $STEP_TIME_ATOL \
+--ckpt_start_step $START_STEPS \
+--max_retries $MAX_RETRIES \
+$CMD_SUFFIX |& tee "$JOB_DIR/train_console.log"
 
 # End timing
 end=$(date +%s)
