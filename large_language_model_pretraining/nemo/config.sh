e840c11e9bdca5c803e5125d474df6bc74daac8a
Michał Marcinkiewicz <43240942+mmarcinkiewicz@users.noreply.github.com>
2025-08-08 18:12:02 +0200

    remove START_EVAL_AT

--- large_language_model_pretraining/nemo/config.sh
+++ large_language_model_pretraining/nemo/config.sh
@@ -78,7 +78,6 @@ export MBS=1
 #     If an empty string is provided (""), then the training will continue until time limit
 #     If we want to save a checkpoint, then this value must be set
 export MAX_STEPS=""
-export START_EVAL_AT="258048" # 18432*FLOOR(0.0026*GBS+12)
 export EVAL_EVERY="18432"
 
 # Experiment: starting steps
