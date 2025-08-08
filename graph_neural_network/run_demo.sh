b0b8457b9678124b13d3997180a0aaa3c11a0225
davidjurado <david.jurado@factored.ai>
2025-08-08 10:50:28 -0500

    Add MLCube implementation for Graph Neural Network  (#762)
    * add mlcube integration
    
    * wip
    
    * add minifed tasks
    
    * wip
    
    * Fix dependencies

---
+++ graph_neural_network/run_demo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set +x
+set -e
+
+# start timing
+start=$(date +%s)
+start_fmt=$(date +%Y-%m-%d\ %r)
+echo "STARTING TIMING RUN AT $start_fmt"
+
+# Set variables
+: "${DATA_DIR:=./igbh/full/processed}"
+: "${LOG_DIR:=./workspace/logs}"
+
+# Handle MLCube parameters
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --data_dir=*)
+        DATA_DIR="${1#*=}"
+        ;;
+    --log_dir=*)
+        LOG_DIR="${1#*=}"
+        ;;
+    *) ;;
+    esac
+    shift
+done
+
+# run benchmark
+echo "running benchmark"
+
+python compress_graph_demo.py --path $DATA_DIR \
+    --dataset_size='tiny' \
+    --layout='CSC' |& tee "$LOG_DIR/demo_console.log"
+
+# end timing
+end=$(date +%s)
+end_fmt=$(date +%Y-%m-%d\ %r)
+echo "ENDING TIMING RUN AT $end_fmt"
